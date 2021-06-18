8fcbb59d42265345c8c768e88dbbfd71681a80c5
Parag Saxena <parag.saxena@confluxsys.com>
2021-06-18 16:22:31 +0530

    app.js updated

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ require("./Startup/routes")(app);
 app.get("/", (req, res) => {
   console.log("Server-Health : OKAY");
 
-  res.status(200).send({ result: "Server-Health:Okay" });
+  res.status(200).send('AgriGrowth Backend: Successfully Up & Running');
 });
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-  console.log(`Server-Started at PORT : http://localhost:${PORT}`);
+  console.log(`Server-Started at PORT : ${PORT}`);
 });
 
 module.exports = server;
